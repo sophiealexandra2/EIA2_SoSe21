524ac54ff7beda005298530d48cfe9b1e1645856
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-03-25 15:11:28 +0100

    .

--- Aufgabe 1/script.js
+++ Aufgabe 1/script.js
@@ -4,7 +4,6 @@ var randompoem;
     let noun = ["Daenerys Targaryen ", "Jon Snow ", "Arya Stark ", "Sansa Stark ", "Eddard Stark ", "Joffrey Baratheon "];
     let verb = ["kauft", "benutzt", "vergisst", "sucht", "liebt", "hasst"];
     let object = [" Schwerter", " Tröne", " Riesen", " Kronen", " Kriege", " Drachen"];
-    //console.log(n);
     for (let i = 6; i >= 1; i--) {
         let finalsentence = getVerse(noun, verb, object);
         console.log(finalsentence);
--- Aufgabe 1/script.ts
+++ Aufgabe 1/script.ts
@@ -3,33 +3,19 @@ let noun: string [] = ["Daenerys Targaryen ", "Jon Snow ", "Arya Stark ", "Sansa
 let verb: string [] = ["kauft", "benutzt", "vergisst", "sucht", "liebt", "hasst"];
 let object: string [] = [" Schwerter", " Tröne", " Riesen", " Kronen", " Kriege", " Drachen"];
 
-//console.log(n);
-
 for (let i: number = 6 ; i >= 1; i--) {
     let finalsentence: string = getVerse(noun, verb, object);
     console.log(finalsentence);
 }
-
-
 function getVerse (_noun: string[], _verb: string[], _object: string[]): string {
 
-
     let randomNumbernoun: number = Math.floor(Math.random() * _noun.length);
-
     let randomNumberverb: number = Math.floor(Math.random() * _noun.length);
-
     let randomNumberobject: number = Math.floor(Math.random() * _noun.length);
-   
     let satzbau: string = _noun[randomNumbernoun] + _verb[randomNumberverb] + _object[randomNumberobject];
-
     _noun.splice(randomNumbernoun, 1);
     _verb.splice(randomNumberverb, 1);
     _object.splice(randomNumberobject, 1);
-
-
-
-
     return satzbau;
-
 }}
 
