09f89762fa5d3155fece90428b3c1e4f9ea04947
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-05-04 17:31:45 +0200

    Circle Aufgabe fertig?

--- Aufgabe Canvas/script.js
+++ Aufgabe Canvas/script.js
@@ -1,24 +1,19 @@
 "use strict";
-let canvas = document.querySelector("canvas");
-let crc2 = canvas.getContext("2d");
-let button = document.getElementById("refresh");
-let refresh = function () {
-    crc2.clearRect(0, 0, 400, 400);
-    let i = 0;
-    for (i = 0; i < 600; i++) {
-        let x = Math.floor(Math.random() * 300);
-        let y = Math.floor(Math.random() * 300);
-        let radius = Math.floor(Math.random() * 20);
-        let r = Math.floor(Math.random() * 255);
-        let g = Math.floor(Math.random() * 255);
-        let b = Math.floor(Math.random() * 255);
-        crc2.beginPath();
-        crc2.arc(x, y, radius, Math.PI * 2, 0, false);
-        crc2.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-        crc2.closePath();
-    }
+window.onload = function () {
+    draw();
 };
-refresh();
-button.addEventListener("click", refresh, false);
+function draw() {
+    let canvas = document.querySelector("canvas");
+    let ctx = canvas.getContext("2d");
+    ctx.fillStyle = "green";
+    for (let i = 0; i < 20; i++) {
+        let x = Math.random() * 500;
+        let y = Math.random() * 500;
+        ctx.beginPath();
+        ctx.arc(x, y, 40, 0, 2 * Math.PI, false);
+        ctx.fill();
+        ctx.stroke();
+        ctx.closePath();
+    }
+}
 //# sourceMappingURL=script.js.map
--- Aufgabe Canvas/script.ts
+++ Aufgabe Canvas/script.ts
@@ -1,31 +1,46 @@
-let canvas: HTMLCanvasElement = document.querySelector("canvas");
-let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
-    
-let button = document.getElementById("refresh");
-let refresh = function () {
 
-      crc2.clearRect(0, 0, 400, 400);
-      let i: number = 0;
-      for (i = 0; i < 600; i++) {
-        let x: number = Math.floor(Math.random() * 300);
-        let y: number = Math.floor(Math.random() * 300);
-        let radius: number = Math.floor(Math.random() * 20);
-    
-        let r: number = Math.floor(Math.random() * 255);
-        let g: number = Math.floor(Math.random() * 255);
-        let b: number = Math.floor(Math.random() * 255);
-    
-        crc2.beginPath();
-        crc2.arc(x, y, radius, Math.PI * 2, 0, false);
-        crc2.fillStyle = "rgba(" + r + "," + g + "," + b + ",1)";
-        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
-        crc2.closePath();
-      }
-    };
-    
-refresh();
-button.addEventListener("click", refresh, false);
-    
+window.onload = function() {
+draw();
+};
+
+function draw () {
+let canvas: HTMLCanvasElement = document.querySelector("canvas");
+let ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+ctx.fillStyle = "green";
+
+for (let i: number = 0; i < 20; i++) { 
+let x: number = Math.random() * 500;
+let y: number = Math.random() * 500;
+ctx.beginPath();
+ctx.arc(x, y, 40, 0, 2 * Math.PI, false);
+ctx.fill();
+ctx.stroke();
+ctx.closePath();
+
+
+
+
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
