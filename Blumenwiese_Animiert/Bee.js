d90f733904e75602845ab3b5ca363b8850aa840b
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-05-21 10:51:00 +0200

    Konzept hochgeladenb

--- Blumenwiese_Animiert/Bee.js
+++ Blumenwiese_Animiert/Bee.js
@@ -1,2 +1,17 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ClassesBlumenwiese;
+(function (ClassesBlumenwiese) {
+    class Bee {
+        contructor(_size) {
+            console.log("Bees works");
+        }
+        moveBy(_timeslice) {
+            console.log("Bees works2");
+        }
+        draw() {
+            console.log("Bees draw");
+        }
+    }
+})(ClassesBlumenwiese || (ClassesBlumenwiese = {}));
 //# sourceMappingURL=Bee.js.map
--- Blumenwiese_Animiert/Bee.ts
+++ Blumenwiese_Animiert/Bee.ts
@@ -1,13 +1,28 @@
+import { deflateRaw } from "node:zlib";
+
 namespace ClassesBlumenwiese {
+class Bee {
+position: Vector;
+velocity: Vector;
+type: number;
+size: number;
 
 
+contructor(_size: number): void {
+
+console.log("Bees works");
+}
+
+moveBy(_timeslice: number): void {
+console.log("Bees works2");
+
+}
 
 
-
-
-
-
-
+draw(): void {
+console.log("Bees draw");
+}
+}
 
 
 
