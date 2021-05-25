b51034e613d221efaae5242b3b864d2048ade32a
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-05-25 12:03:29 +0200

    bee

--- Blumenwiese_Animiert/main.js
+++ Blumenwiese_Animiert/main.js
@@ -109,6 +109,46 @@ var ClassesBlumenwiese;
             x++;
         }, 25);
     }
+    //Anfang Biene
+    const circle = function (x, y, radius, fillCircle) {
+        ClassesBlumenwiese.ctx.beginPath();
+        ClassesBlumenwiese.ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+        if (fillCircle) {
+            ClassesBlumenwiese.ctx.fill();
+        }
+        else {
+            ClassesBlumenwiese.ctx.stroke();
+        }
+    };
+    function drawBee(x, y) {
+        ClassesBlumenwiese.ctx.lineWidth = 2;
+        ClassesBlumenwiese.ctx.strokeStyle = "black";
+        ClassesBlumenwiese.ctx.fillStyle = "gold";
+        circle(x, y, 8, true);
+        circle(x, y, 8, false);
+        circle(x - 5, y - 11, 5, false);
+        circle(x + 5, y - 11, 5, false);
+        circle(x - 2, y - 1, 2, false);
+        circle(x + 2, y - 1, 2, false);
+    }
+    function biene(coordinate) {
+        let offset = Math.random() * 4 - 2;
+        coordinate += offset;
+        if (coordinate > 400) {
+            coordinate = 400;
+        }
+        if (coordinate < 0) {
+            coordinate = 0;
+        }
+        return coordinate;
+    }
+    let x = 400;
+    let y = 700;
+    setInterval(function () {
+        drawBee(x, y);
+        x = biene(x);
+        y = biene(y);
+    }, 80);
     //Mond - Jirkas Code, abgeändert also random weggemacht :)
     function drawMoon() {
         let r1 = 30;
--- Blumenwiese_Animiert/main.ts
+++ Blumenwiese_Animiert/main.ts
@@ -147,7 +147,56 @@ namespace ClassesBlumenwiese {
 }
 
 
+//Anfang Biene
 
+  const circle = function (x, y, radius, fillCircle) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
+  if (fillCircle) {
+      ctx.fill();
+  } else {
+      ctx.stroke();
+  }
+
+};
+  function drawBee (x, y) {
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "black";
+  ctx.fillStyle = "gold";
+
+  circle(x, y, 8, true);
+  circle(x, y, 8, false);
+  circle(x - 5, y - 11, 5, false);
+  circle(x + 5, y - 11, 5, false);
+  circle(x - 2, y - 1, 2, false);
+  circle(x + 2, y - 1, 2, false);
+}
+
+  function biene (coordinate) {
+  let offset = Math.random() * 4 - 2;
+  coordinate += offset;
+
+  if (coordinate > 400){
+      coordinate = 400;
+  }
+  if (coordinate < 0) {
+      coordinate = 0;
+  }
+  return coordinate;
+}
+
+
+  let x: number = 400;
+  let y: number = 700;
+
+  setInterval(function () {
+ 
+
+  drawBee(x, y);
+  x = biene(x);
+  y = biene(y);
+
+},            80);
 
 
 
