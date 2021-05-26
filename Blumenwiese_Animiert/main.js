d70e927c09e593c8acbaf7bfba01c82a91370ca9
sophiealexandra2 <72866151+sophiealexandra2@users.noreply.github.com>
2021-05-26 13:34:34 +0200

    .

--- Blumenwiese_Animiert/main.js
+++ Blumenwiese_Animiert/main.js
@@ -11,6 +11,7 @@ var ClassesBlumenwiese;
     ClassesBlumenwiese.canvas.height = windowHeight;
     ClassesBlumenwiese.canvas.width = 1300;
     ClassesBlumenwiese.canvas.height = 700;
+    let saveBackgroundData;
     window.addEventListener("load", handleLoad);
     function handleLoad() {
         if (!ClassesBlumenwiese.canvas)
@@ -78,47 +79,10 @@ var ClassesBlumenwiese;
             drawCircle(x % 1300, 70);
             x++;
         }, 25);
+        console.log(ClassesBlumenwiese.ctx.getImageData(100, 200, 100, 100));
     }
     //Anfang Biene
-    const circle = function (x, y, radius, fillCircle) {
-        ClassesBlumenwiese.ctx.beginPath();
-        ClassesBlumenwiese.ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-        if (fillCircle) {
-            ClassesBlumenwiese.ctx.fill();
-        }
-        else {
-            ClassesBlumenwiese.ctx.stroke();
-        }
-    };
-    function drawBee(x, y) {
-        ClassesBlumenwiese.ctx.lineWidth = 2;
-        ClassesBlumenwiese.ctx.strokeStyle = "black";
-        ClassesBlumenwiese.ctx.fillStyle = "gold";
-        circle(x, y, 8, true);
-        circle(x, y, 8, false);
-        circle(x - 5, y - 11, 5, false);
-        circle(x + 5, y - 11, 5, false);
-        circle(x - 2, y - 1, 2, false);
-        circle(x + 2, y - 1, 2, false);
-    }
-    function biene(coordinate) {
-        let offset = Math.random() * 4 - 2;
-        coordinate += offset;
-        if (coordinate > 400) {
-            coordinate = 400;
-        }
-        if (coordinate < 0) {
-            coordinate = 0;
-        }
-        return coordinate;
-    }
-    let x = 400;
-    let y = 700;
-    setInterval(function () {
-        drawBee(x, y);
-        x = biene(x);
-        y = biene(y);
-    }, 40);
+    ClassesBlumenwiese.ctx.putImageData(saveBackgroundData, 100, 100);
     //Mond - Jirkas Code, abgeändert also random weggemacht :)
     function drawMoon() {
         let r1 = 30;
--- Blumenwiese_Animiert/main.ts
+++ Blumenwiese_Animiert/main.ts
@@ -13,6 +13,8 @@ namespace ClassesBlumenwiese {
   canvas.height = windowHeight;
   canvas.width = 1300;
   canvas.height = 700;
+  let saveBackgroundData: ImageData;
+
 
   window.addEventListener("load", handleLoad);
 
@@ -77,6 +79,7 @@ namespace ClassesBlumenwiese {
     ctx.restore();
 }
 
+
 //Anfang Cloud Moving
 
   function MovingCloud (): void {
@@ -102,66 +105,19 @@ namespace ClassesBlumenwiese {
     drawCircle(x % 1300, 70);
     x++;
   },            25);
+    console.log(ctx.getImageData(100, 200, 100, 100));
+
 }
 
 
 //Anfang Biene
 
-  const circle = function (x, y, radius, fillCircle): void {
-  ctx.beginPath();
-  ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-  if (fillCircle) {
-      ctx.fill();
-  } else {
-      ctx.stroke();
-  }
-
-};
-  function drawBee (x, y) {
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "black";
-  ctx.fillStyle = "gold";
-
-  circle(x, y, 8, true);
-  circle(x, y, 8, false);
-  circle(x - 5, y - 11, 5, false);
-  circle(x + 5, y - 11, 5, false);
-  circle(x - 2, y - 1, 2, false);
-  circle(x + 2, y - 1, 2, false);
-}
-
-  function biene (coordinate) {
-  let offset: number = Math.random() * 4 - 2;
-  coordinate += offset;
-
-  if (coordinate > 400) {
-      coordinate = 400;
-  }
-  if (coordinate < 0) {
-      coordinate = 0;
-  }
-  return coordinate;
-}
-
-
-  let x: number = 400;
-  let y: number = 700;
-
-  setInterval(function (): void {
- 
-
-  drawBee(x, y);
-  x = biene(x);
-  y = biene(y);
-
-},            40);
 
 
 
 
 
-
-
+ctx.putImageData(saveBackgroundData, 100, 100);
 
 
 
